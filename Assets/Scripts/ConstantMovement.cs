6ea26b4c39ff1f0b7baaf3c78c17b6f903a29513
kenny-skaggs <skaggskd88@gmail.com>
2023-03-05 12:11:18 -0800

    added vortex

--- Assets/Scripts/ConstantMovement.cs
+++ Assets/Scripts/ConstantMovement.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ConstantMovement : MonoBehaviour
 {
     public Vector2 movement;
+    public float rotation;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movement.x * Time.deltaTime, movement.y * Time.deltaTime, 0);
+        if (movement.x != 0 || movement.y != 0) {
+            transform.Translate(movement.x * Time.deltaTime, movement.y * Time.deltaTime, 0);
+        }
+
+        if (rotation != 0) {
+            transform.Rotate(
+                new Vector3(0, 0, rotation * Time.deltaTime)
+            );
+        }
     }
 }
